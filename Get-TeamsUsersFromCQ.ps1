c916f3aba0c7d399406380e7c88373de954e15bb
rbutor <92458249+rbutor@users.noreply.github.com>
2022-08-28 23:06:31 +0300

    Update Get-TeamsUsersFromCQ.ps1
    Add Phone and SIP URI

--- Get-TeamsUsersFromCQ.ps1
+++ Get-TeamsUsersFromCQ.ps1
@@ -1,7 +1,18 @@
-Import-Module MicrosoftTeams
-Connect-MicrosoftTeams
+#Connect-MicrosoftTeams 
+# | Where-Object {$_.Name -eq "Оперативник Нефтяная"} 
+$cs = Get-CsCallQueue | select name, ApplicationInstances, Tenantid, Identity, users, agents
 
-$cs = Get-CsCallQueue | select Tenantid, name, users, agents
+foreach ($sipid in $cs) 
+    {
+    
+    foreach ($sip in $sipid.ApplicationInstances) 
+        {
+        $outsip = ((get-csonlineuser -identity $sip).SipProxyAddress)
+        }
+        $sipid.Identity = $outsip        
+    }
+
+
 
 foreach ($x in $cs) 
     {
@@ -10,9 +21,9 @@ foreach ($x in $cs)
         {
         $out += [pscustomobject]@((get-csonlineuser -identity $u).sipaddress)
         }
-        $x.users = $out
-        
+        $x.users = $out        
     }
+
 foreach ($v in $cs) 
     {
     $out2 = @()
@@ -20,7 +31,16 @@ foreach ($v in $cs)
         {
         $out2 += [pscustomobject]@((get-csonlineuser -identity $u2).sipaddress)
         }
-        $x.agents = $out2
-        
+        $v.agents = $out2        
     }
-$cs
+foreach ($appid in $cs) 
+    {
+    
+    foreach ($phone in $appid.ApplicationInstances) 
+        {
+        $outappid = ((get-csonlineuser -identity $phone).LineURI)
+        }
+        $appid.ApplicationInstances = $outappid  
+    }
+
+$cs | ConvertTo-Json
